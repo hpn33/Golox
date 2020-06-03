30dd3f2927fee9c8d92c72ea9e235b73135b8c9a
hamed <hamed.hpn332@gmail.com>
2020-06-03 23:49:53 +0430

    add visitor pattern to ast

--- core/ast/Expr.gd
+++ core/ast/Expr.gd
@@ -1,19 +1,32 @@
 extends Object
 
+
 class Expr:
+	
 	func _init(): pass
+	
+	func visit_Binary_Expr(expr: Binary): pass
+	func visit_Grouping_Expr(expr: Grouping): pass
+	func visit_Literal_Expr(expr: Literal): pass
+	func visit_Unary_Expr(expr: Unary): pass
+	
+	func accept(visitor): pass
+
 
 class Binary:
 	extends Expr
 	
-	var left
+	var left: Expr
 	var operator: Token
 	var right: Expr
 	
-	func _init(left, operator: Token, right: Expr):
+	func _init(left: Expr, operator: Token, right: Expr):
 		self.left = left
 		self.operator = operator
 		self.right = right
+	
+	func accept(visitor):
+		return visitor.visit_Binary_Expr(self)
 
 
 class Grouping:
@@ -23,6 +36,9 @@ class Grouping:
 	
 	func _init(expression: Expr):
 		self.expression = expression
+	
+	func accept(visitor):
+		return visitor.visit_Grouping_Expr(self)
 
 
 class Literal:
@@ -32,6 +48,9 @@ class Literal:
 	
 	func _init(value):
 		self.value = value
+	
+	func accept(visitor):
+		return visitor.visit_Literal_Expr(self)
 
 
 class Unary:
@@ -43,4 +62,7 @@ class Unary:
 	func _init(operator: Token, right: Expr):
 		self.operator = operator
 		self.right = right
+	
+	func accept(visitor):
+		return visitor.visit_Unary_Expr(self)
 
--- tool/ast/GenerateAst.gd
+++ tool/ast/GenerateAst.gd
@@ -1,9 +1,15 @@
 extends Node
 
 
+#		[
+#			"Binary   : Expr left, Token operator, Expr right",
+#			"Grouping : Expr expression",                      
+#			"Literal  : Object value",                         
+#			"Unary    : Token operator, Expr right"            
+#		],
 var struc := {
 	'Binary': {
-		'left': '',
+		'left': 'Expr',
 		'operator': 'Token',
 		'right': 'Expr'
 	},
@@ -26,37 +32,38 @@ func _ready():
 	
 	
 	define_ast('core/ast', "Expr", struc)
-#		[
-#			"Binary   : Expr left, Token operator, Expr right",
-#			"Grouping : Expr expression",                      
-#			"Literal  : Object value",                         
-#			"Unary    : Token operator, Expr right"            
-#		],
-		
+	
 
 func define_ast(output_dir: String,  base_name: String, types: Dictionary):
 
 	var writer := TextWriter.new()
 	
 	writer.add('extends Object')\
-		.new_line()\
+		.add_line(2)\
 		.new_line('class %s:' % base_name)\
 		.add_level()\
+		.add_line()\
 		.new_line('func _init(): pass')\
+		.add_line()\
 		.sub_level()\
-		.new_line()\
 		.done()
 	
+	define_visitor(writer, base_name, types)
+	
+	
+	# The base accept() method.
+	writer.add_level()\
+		.add_line()\
+		.new_line("func accept(visitor): pass")\
+		.sub_level()\
+		.done()
+	
+	writer.add_line(2).done()
 	
 	# The AST classes.
-	for i in types.size():
-#		var clas_name = type.split(":")[0]
-#		var fields = type.split(":")[1]
+	for clas_name in types.keys():
 		
-		var clas_name = types.keys()[i]
-		var fields = types.values()[i]
-		
-		define_type(writer, base_name, clas_name, fields)
+		define_type(writer, base_name, clas_name, types[clas_name])
 	
 	
 	
@@ -74,6 +81,18 @@ func define_ast(output_dir: String,  base_name: String, types: Dictionary):
 	
 	$TextEdit.text = writer.text
 	
+
+
+func define_visitor(writer: TextWriter, base_name: String, types: Dictionary):
+	
+	writer.add_level()
+	
+	for type_name in types.keys():
+		
+		writer.new_line('func visit_%s_%s(%s: %s): pass' % [type_name, base_name, base_name.to_lower(), type_name])
+	
+	writer.sub_level()
+
 
 func define_type(writer: TextWriter, base_name: String, clas_name: String, field_list: Dictionary):
 	writer.new_line("class %s:" % clas_name)\
@@ -118,11 +137,15 @@ func define_type(writer: TextWriter, base_name: String, clas_name: String, field
 		
 		writer.new_line("self.%s = %s" % [key, key]).done()
 	
+	writer.sub_level().add_line().done()
 	
-	writer.sub_level(2)\
-		.new_line().new_line()\
+	# Visitor pattern.
+	writer.new_line("func accept(visitor):")\
+		.add_level()\
+		.new_line("return visitor.visit_%s_%s(self)" % [clas_name, base_name])\
+		.sub_level(2)\
+		.add_line(2)\
 		.done()
-
 
 
 
@@ -141,6 +164,16 @@ class TextWriter:
 	func new_line(_text = '') -> TextWriter:
 		
 		return add('\n' + tabs() + _text)
+	
+	
+	func add_line(num: int = 1) -> TextWriter:
+		
+		var line = ''
+		
+		for i in num:
+			line += '\n' + tabs()
+		
+		return add(line)
 	
 	
 	func add_level(num: int = 1) -> TextWriter:
